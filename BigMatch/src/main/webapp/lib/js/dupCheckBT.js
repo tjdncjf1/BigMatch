33f2b45ec6bb475a087110559ce3ba638163beee
Jominho <minho0406@naver.com>
2016-05-10 16:17:19 +0900

    05-10 조민호 home 에서 swipe시 dupCheck

---
+++ BigMatch/src/main/webapp/lib/js/dupCheckBT.js
@@ -0,0 +1,49 @@
+/**
+ * Program    : dupCheckSwipe.js
+   Date       : 2016-05-10
+   Description: 홈화면 button click 시 event (POSSIBLE_div, REGISTERED_div, EXISTENCE_div) 속성 을 받아 toast 출력.
+   Modify     : 서우철
+   History    :  
+ * 
+ */
+
+
+
+function dupCheckBT(){
+	
+	 $.ajax({
+		url: baseUrl + '/matchDupCheck.do/' +userseq,
+		method:'GET',
+		headers: { 
+			"Accept" : "application/json",
+			"Content-Type" : "application/json"
+		},
+		success : function(data) {
+			console.log("받아온 결과 data값 : "+data);
+			
+			if(data == "POSSIBLE_ACTION") { // 매치등록 가능상태
+				console.log('등록 가능');
+				location.href = "../matchlist/addMatch.html"; 
+			} else if(data == "ALREADY_REGISTERED")	{ // 이미 매치를 등록한 상태
+				 $.toast({
+	   	   		    //heading: 'Error',
+	   	   		    text: '이미 등록한 매칭이 있습니다.',
+	   	   			position: 'bottom-left',
+	   	   		    icon: 'error',
+	   	   			textAlign: 'center'
+	   				});
+			} else if(data == "ALREADY_APPENDED")	{ // 이미 다른 매치에 신청한 상태
+				$.toast({
+	   	   		    //heading: 'Error',
+	   	   		    text: '이미 신청한 매칭이 있습니다.',
+	   	   			position: 'bottom-left',
+	   	   		    icon: 'error',
+	   	   		  	textAlign: 'center'
+	   				}); 
+			} else if (data == "APPENDER_EXISTENCE") { // 내가 등록한 매치에 신청자가 있는 상태
+				view=3;
+	   				location.href='../main/home.html?viewNum=' + view + '&toastBoolean=0';				
+			}
+		}
+	}); 
+}
---
+++ BigMatch/src/main/webapp/lib/js/dupCheckSwipe.js
@@ -0,0 +1,52 @@
+/**
+ * Program    : dupCheckSwipe.js
+   Date       : 2016-05-10
+   Description: 홈화면 swiper 시 ajax에 따라 (POSSIBLE_div, REGISTERED_div, EXISTENCE_div) 속성 을 받아 matchPage UI 변경.
+   Modify     : 조민호
+   History    :  
+ * 
+ */
+
+
+function dupCheckSwipe() {
+	$.ajax({
+				url : baseUrl + '/matchDupCheck.do/' + userseq,
+				method : 'GET',
+				headers : {
+					"Accept" : "application/json",
+					"Content-Type" : "application/json"
+				},
+				success : function(data) {
+					console.log("받아온값 받아온 결과 data값 (home에서): " + data);
+
+					// data test ///////////////////////
+					// data = "ALREADY_REGISTERED";
+					// console.log("변경시킨 값 : "+data);
+					// ////////////////////////////
+
+					// 결과에 따라 다른 UI 구성
+					// DB에서 받아온 data 에 따라 구성되어 있는 <div>들을 display : none ->
+					// block 으로 변경 시킴
+
+					if (data == "POSSIBLE_ACTION") { // 매치등록 가능상태
+						document.getElementById("POSSIBLE_div").style.display = "block";
+						document.getElementById("REGISTERED_div").style.display = "none";
+						document.getElementById("EXISTENCE_div").style.display = "none";
+					} else if (data == "ALREADY_REGISTERED") { // 이미 매치를 등록한 상태
+						document.getElementById("POSSIBLE_div").style.display = "none";
+						document.getElementById("REGISTERED_div").style.display = "block";
+						document.getElementById("EXISTENCE_div").style.display = "none";
+					} else if (data == "ALREADY_APPENDED") { // 이미 다른 매치에 신청한
+																// 상태
+						document.getElementById("POSSIBLE_div").style.display = "none";
+						document.getElementById("REGISTERED_div").style.display = "block";
+						document.getElementById("EXISTENCE_div").style.display = "none";
+					} else if (data == "APPENDER_EXISTENCE") { // 내가 등록한 매치에
+																// 신청자가 있는 상태
+						document.getElementById("POSSIBLE_div").style.display = "none";
+						document.getElementById("REGISTERED_div").style.display = "none";
+						document.getElementById("EXISTENCE_div").style.display = "block";
+					}
+				}
+			});
+}
